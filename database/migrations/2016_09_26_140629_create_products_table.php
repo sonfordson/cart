23e0cb9b7611ff6717494aeb6d7794dbed1a9e6f
sonford onyango <sonfordonyango@gmail.com>
2016-09-26 07:16:13 -0700

    initial commit

---
+++ database/migrations/2016_09_26_140629_create_products_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+   
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('imagepath');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+@section('title')
+      Sonford  Shopping Cart
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t09.deviantart.net/2gSWw0G_08X9jE9-FKMQjo7W2wQ=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre04/b92f/th/pre/f/2007/037/1/9/hp_and_the_deathly_hallows_by_harry_potter_spain.jpg" alt="..." style="max-height: 200px" class="img-responsive">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </div>
+                    <div class="clearfix">
+                        <div class="pull-left">Price $ 567</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                 </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t09.deviantart.net/2gSWw0G_08X9jE9-FKMQjo7W2wQ=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre04/b92f/th/pre/f/2007/037/1/9/hp_and_the_deathly_hallows_by_harry_potter_spain.jpg" alt="..." style="max-height: 200px" class="img-responsive">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="clearfix">
+                    <div class="pull-left">Price $ 567</div>
+                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t09.deviantart.net/2gSWw0G_08X9jE9-FKMQjo7W2wQ=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre04/b92f/th/pre/f/2007/037/1/9/hp_and_the_deathly_hallows_by_harry_potter_spain.jpg" alt="..." style="max-height: 200px" class="img-responsive">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="clearfix">
+                    <div class="pull-left">Price $ 567</div>
+                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t09.deviantart.net/2gSWw0G_08X9jE9-FKMQjo7W2wQ=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre04/b92f/th/pre/f/2007/037/1/9/hp_and_the_deathly_hallows_by_harry_potter_spain.jpg" alt="..." style="max-height: 200px" class="img-responsive">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="clearfix">
+                    <div class="pull-left">Price $ 567</div>
+                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t09.deviantart.net/2gSWw0G_08X9jE9-FKMQjo7W2wQ=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre04/b92f/th/pre/f/2007/037/1/9/hp_and_the_deathly_hallows_by_harry_potter_spain.jpg" alt="..." style="max-height: 200px" class="img-responsive">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="clearfix">
+                    <div class="pull-left">Price $ 567</div>
+                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t09.deviantart.net/2gSWw0G_08X9jE9-FKMQjo7W2wQ=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre04/b92f/th/pre/f/2007/037/1/9/hp_and_the_deathly_hallows_by_harry_potter_spain.jpg" alt="..." style="max-height: 200px" class="img-responsive">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="clearfix">
+                    <div class="pull-left">Price $ 567</div>
+                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
